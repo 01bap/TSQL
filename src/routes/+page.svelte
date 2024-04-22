369f8867a02196c68365242d6d5eabe60f373311
01bap <olbap007@icloud.com>
2024-04-22 22:48:06 +0200

    Added TailwindCSS and DaisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style lang="postcss">
+  :global(html) {
+    /* background-color: theme(colors.slate.500); */
+  }
+</style>
+
+<div class="hero min-h-screen" style="background-image: url(bgi.jpg);" data-theme="night">
+  <div class="hero-overlay bg-opacity-60"></div>
+  <div class="hero-content text-center text-neutral-content">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">Welcome to ...</h1>
+      <button class="btn btn-primary text-xl">Sign in</button>
+    </div>
+  </div>
+</div>
